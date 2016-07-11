242779dea353bf96b00f438d7d7e61b4f128b946
Stealthy89 <stealthy89@rambler.ru>
2016-07-12 01:22:52 +0300

    Added changes after code review

--- src/com/yachenya/list/MyArrayList.java
+++ src/com/yachenya/list/MyArrayList.java
@@ -8,12 +8,9 @@ import java.util.Arrays;
  * @author ayachenya
  */
 
-//1. Change github view;
-// 2. myarraylist - rework methods and tests. look through the commets.
 class MyArrayList implements List {
 
     private static final int CAPACITY = 15;
-    //array  +
     private Object[] array;
     private int size;
 
@@ -31,26 +28,22 @@ class MyArrayList implements List {
         return size == 0;
     }
 
-    //indexOf() +
     @Override
     public boolean contains(Object o) {
         return indexOf(o) > 0;
     }
-
-    //add( , )  +
 
     @Override
     public void add(Object o) {
         add(size, o);
     }
 
-    // 2, 3, 5, 6 - remove 3 -> 2, 5, 5, 6
     @Override
     public boolean remove(Object o) {
         for (int i = 0; i < size; i++) {
             if (o.equals(array[i])) {
                 System.arraycopy(array, i + 1, array, i, size - i - 1);
-                size--;
+                array[size--] = null;
                 return true;
             }
         }
@@ -65,17 +58,16 @@ class MyArrayList implements List {
         size = 0;
     }
 
-    private int validateIndex(int index) {
-        if (index > size) {
+    private void validateIndex(int index) {
+        if (index > size || index < 0) {
             throw new IndexOutOfBoundsException();
-        } else {
-            return index;
         }
     }
 
     @Override
     public Object get(int index) {
-        return array[validateIndex(index)];
+        validateIndex(index);
+        return array[index];
     }
 
     @Override
@@ -85,35 +77,35 @@ class MyArrayList implements List {
         return result;
     }
 
-    /*if array is full -> newarray=array*2*/
-    //without for
-    //2, 3, 4 - add(5, 1) -> exception (size = 3) max index = 3 - 2, 3, 4, 1
-    @Override
-    public void add(int index, Object element) {
-        try {
-            validateIndex(index);
-            System.arraycopy(array, index, array, index + 1, size + 1);
-            array[index] = element;
-            size++;
-        } catch (IndexOutOfBoundsException e) {
-            Object[] newArray = new Object[CAPACITY * 2];
-            System.arraycopy(array, 0, newArray, 0, size);
-            newArray[index] = element;
-            size++;
-        }
+    private void increaseCapacity() {
+        Object[] newArray = new Object[CAPACITY * 2];
+        System.arraycopy(array, 0, newArray, 0, size);
+        array = newArray;
     }
 
-    //wrong
+    @Override
+    public void add(int index, Object element) {
+        validateIndex(index);
+        if (size >= CAPACITY) {
+            increaseCapacity();
+        }
+        System.arraycopy(array, index, array, index + 1, size - index);
+        array[index] = element;
+        size++;
+    }
+
     @Override
     public Object remove(int index) {
         validateIndex(index);
+
         Object result = array[index];
-        System.arraycopy(array, index + 1, array, index, size - index - 1);
-        size--;
+        if ((size - index - 1) > 0) {
+            System.arraycopy(array, index + 1, array, index, size - index - 1);
+        }
+        array[--size] = null;
         return result;
     }
 
-    // str.equals("Hello")  -  "Hello".equals(str) +
     @Override
     public int indexOf(Object o) {
         for (int i = 0; i < size; i++) {
@@ -134,7 +126,6 @@ class MyArrayList implements List {
         return -1;
     }
 
-    //toString() +
     @Override
     public String toString() {
         return Arrays.toString(Arrays.copyOf(array, size));
--- test/com/yachenya/list/MyArrayListTests.java
+++ test/com/yachenya/list/MyArrayListTests.java
@@ -33,7 +33,6 @@ public class MyArrayListTests extends MyArrayList {
         Assert.assertEquals(false, arrayList.contains(10));
     }
 
-
     @Test
     public void testRemove() throws Exception {
         arrayList.add("S");
@@ -42,7 +41,6 @@ public class MyArrayListTests extends MyArrayList {
         arrayList.add("O");
         Assert.assertEquals(true, arrayList.remove("S"));
         Assert.assertEquals(false, arrayList.remove("A"));
-        System.out.println(arrayList);
     }
 
     @Test
@@ -58,6 +56,12 @@ public class MyArrayListTests extends MyArrayList {
         Assert.assertEquals(1, arrayList.get(0));
     }
 
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetNegative() throws Exception {
+        arrayList.get(1);
+        arrayList.get(-1);
+    }
+
     @Test
     public void testSet() throws Exception {
         arrayList.add(2);
@@ -66,7 +70,6 @@ public class MyArrayListTests extends MyArrayList {
 
         Assert.assertEquals("A", arrayList.set(2, "B"));
         Assert.assertEquals(2, arrayList.set(0, 3));
-        System.out.println(arrayList);
     }
 
     @Test
@@ -74,19 +77,17 @@ public class MyArrayListTests extends MyArrayList {
         arrayList.add(10);
         arrayList.add(12);
         arrayList.add(0, 1);
-        System.out.println(arrayList);
-  /*      Assert.assertEquals(1, arrayList.get(0));
-        Assert.assertEquals(12, arrayList.get(1));
-       Assert.assertEquals(12, arrayList.get(2));*/
+        Assert.assertEquals(1, arrayList.get(0));
+        Assert.assertEquals(10, arrayList.get(1));
+        Assert.assertEquals(12, arrayList.get(2));
     }
 
     @Test
     public void testRemoveByIndex() throws Exception {
         arrayList.add(1);
         arrayList.add(12);
-        System.out.println(arrayList);
         Assert.assertEquals(1, arrayList.remove(0));
-        System.out.println(arrayList);
+        Assert.assertEquals(12, arrayList.get(0));
     }
 
     @Test
@@ -103,5 +104,38 @@ public class MyArrayListTests extends MyArrayList {
         arrayList.add(12);
         arrayList.add(1);
         Assert.assertEquals(2, arrayList.lastIndexOf(1));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testValidateIndexNegative() throws Exception {
+     arrayList.add(-1, 1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testValidateIndexHuge() throws Exception {
+        arrayList.add(0,1);
+        arrayList.add(1,2);
+        arrayList.add(4, 3);
+    }
+    @Test
+    public void testIncreaseCapacity() throws Exception {
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.add(5);
+        arrayList.add(6);
+        arrayList.add(7);
+        arrayList.add(8);
+        arrayList.add(9);
+        arrayList.add(10);
+        arrayList.add(11);
+        arrayList.add(12);
+        arrayList.add(13);
+        arrayList.add(14);
+        arrayList.add(15);
+        Assert.assertEquals(15, arrayList.size());
+        arrayList.add(16);
+        Assert.assertEquals(16, arrayList.size());
     }
 }
